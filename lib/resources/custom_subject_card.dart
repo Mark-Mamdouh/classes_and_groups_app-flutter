6fb153277c4e7224ecd6e2d2174083770b7898d3
Mark-Mamdouh <eng.mark.mamdouh78@gmail.com>
2022-05-19 16:15:29 +0200

    added vertical and horizontal listviews

--- lib/resources/custom_subject_card.dart
+++ lib/resources/custom_subject_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:scrolling_tabs/constants.dart';
+import '../utilities/constants.dart';
 
 class CustomSubjectCard extends StatelessWidget {
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:classes_and_groups_app/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../resources/custom_subject_card.dart';
+import '../resources/custom_teacher_card.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,8 +12,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> subjects = [];
+
+    subjects.add(CustomSubjectCard(true, "item1"));
+
+    for(int i = 2; i < 7; i++) {
+      subjects.add(CustomSubjectCard(false, "item$i"));
+    }
+
+    List<Widget> teachers = [];
+    for(int i = 1; i <= 20; i++) {
+      teachers.add(CustomTeacherCard("Teacher$i", "Starting Date"));
+    }
+
     return MaterialApp(
       home: Scaffold(
         // be aware of notches
@@ -33,14 +53,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       SizedBox(
-                        height: 80,
+                        height: 90,
                       ),
                       Padding(
                         padding: EdgeInsets.only(left: 25),
                         child: Text(
                           "Classes and Groups",
                           style: TextStyle(
-                            fontSize: 25,
+                            fontSize: 30,
                             color: secondary_color,
                             fontWeight: FontWeight.bold
                           ),
@@ -55,6 +75,33 @@ class _HomeScreenState extends State<HomeScreen> {
                 flex: 4,
                 child: Container(
                   color: body_color,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: subjects,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 15, bottom: 10),
+                        child: Text(
+                          "List Of Courses",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView(
+                          scrollDirection: Axis.vertical,
+                          children: teachers,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
